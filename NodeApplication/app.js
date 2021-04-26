86d25cbfe54a8422bbb7550c3f51bee72824b42d
James M <massengillejames@gmail.com>
2021-04-26 18:56:59 -0500

    Updated front-end

--- NodeApplication/app.js
+++ NodeApplication/app.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = 4000
-//import main from './public/js/createTrans.js';
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//fabric API
 const { Gateway, Wallets } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
@@ -30,9 +30,10 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('main', {layout: false});
+    
 });
 
-app.post('/invoke', async (req, res) => {
+app.get('/query', async (req, res) => {
 
     try {
         // load the network configuration
@@ -65,9 +66,21 @@ app.post('/invoke', async (req, res) => {
         // Evaluate the specified transaction.
         // queryCar transaction - requires 1 argument, ex: ('queryledger, asset1')
         // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-        const result = await contract.evaluateTransaction('queryLedger');
-        JSON.stringify(result);
-        res.render('main', {test: result, layout: false});
+        //const result = await contract.evaluateTransaction('queryLedger');
+        let result = await contract.evaluateTransaction('queryLedger');
+        result = JSON.parse(result);
+
+
+        let string = '';
+        let i;
+        for (i = 0; i < await contract.evaluateTransaction('getAssetNum'); i++)
+        {
+            string = string + '\n' + result[i].Key + ' Source: ' + result[i].Record.Source + " Destination: " + result[i].Record.Destination + ' Energy: ' + result[i].Record.Energy + ' Price: ' + result[i].Record.Price;
+
+        }
+
+        console.log(string);
+        res.render('main', {test: string, title: 'TRANSACTIONS', layout: false});
         console.log('transaction without toString()\n' + result);
 
         
@@ -81,5 +94,62 @@ app.post('/invoke', async (req, res) => {
         console.error(`Failed to evaluate transaction: ${error}`);
         process.exit(1);
     }
+
+    app.post('/invoke', async (req,res) => {
+        console.log('hello from invoke');
+        //let assetid = args[0];
+        let source = 'Charging Station';
+        let destination = req.body.nameField;
+        let energy = req.body.energyField;
+        //let price = energy * .13;
+
+
+        try {
+            // load the network configuration
+            const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+            let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = await Wallets.newFileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+
+            // Check to see if we've already enrolled the user.
+            const identity = await wallet.get('appUser');
+            if (!identity) {
+                console.log('An identity for the user "appUser" does not exist in the wallet');
+                console.log('Run the registerUser.js application before retrying');
+                return;
+            }
+
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+            // Get the network (channel) our contract is deployed to.
+            const network = await gateway.getNetwork('mychannel');
+
+            // Get the contract from the network.
+            const contract = network.getContract('evtrans');
+
+            let count = await contract.evaluateTransaction('getAssetNum');
+            count = parseInt(count) + 1;
+
+            let assetid = 'asset' + count;
+
+            // Submit the specified transaction.
+            await contract.submitTransaction('createTrans', assetid, source, destination, energy);
+            
+            console.log('Transaction has been submitted');
+            res.render('main', {title: 'Transaction submitted successfully', layout: false});
+
+            // Disconnect from the gateway.
+            gateway.disconnect();
+
+        } catch (error) {
+            console.error(`Failed to submit transaction: ${error}`);
+            process.exit(1);
+        }
+    });
 });
 
